fc58c1cac3323216e47ce3299cf4b2f7c462e4c9
fagomezomil <76710903+fagomezomil@users.noreply.github.com>
2024-07-11 21:01:30 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   purge:{
     options: {
       content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-      safelist: ['bg-emerald-200', 'bg-purple-200', 'bg-red-200', 'bg-blue-200', 'bg-yellow-200', 'bg-cyan-200', 'bg-green-200', 'bg-gray-200', 'bg-yellow-200', 'bg-pink-200', 'bg-amber-200', 'bg-orange-200', 'bg-violet-200', 'bg-stone-200', 'bg-sky-200', 'bg-slate-200', 'bg-indigo-200', 'bg-black-200'], // Agrega aquí las clases de colores que necesitas mantener
+      safelist: ['bg-emerald-200', 'bg-purple-200', 'bg-red-200', 'bg-blue-200', 'bg-yellow-200', 'bg-cyan-200', 'bg-green-200', 'bg-gray-200', 'bg-yellow-200', 'bg-pink-200', 'bg-amber-200', 'bg-orange-200', 'bg-violet-200', 'bg-stone-200', 'bg-sky-200', 'bg-slate-200', 'bg-indigo-200', 'bg-black-200'],
     },
   },
   theme: {
